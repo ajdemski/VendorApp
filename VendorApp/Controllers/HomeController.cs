4d7fcd4c4537d5a9575570a8c7bd31f84268c7b6
Aaron Demski <ajdemski@frontier.com>
2023-07-22 11:24:28 -0700

    Adjusted controllers/going to finish tomorrow.

--- VendorApp/Controllers/HomeController.cs
+++ VendorApp/Controllers/HomeController.cs
@@ -5,31 +5,30 @@ namespace VendorApp.Controllers;
 
 public class HomeController : Controller
 {
-    [HttpGet("/vendors")]
     public ActionResult Index()
     {
-        Vendor vendor1 = new("Jammoma's Jerk");
-        Vendor vendor2 = new("O-Keto Do-Keto");
-        Vendor vendor3 = new("Durpy's Slurpies");
-        Order order1 = new("BBQ Sauce");
-        Order order2 = new("Chicken");
-        Order order3 = new("Rice");
-        vendor1.AddOrder(order1);
-        vendor1.AddOrder(order2);
-        vendor1.AddOrder(order3);
         List<Vendor> allVendors = Vendor.GetAll();
         return View(allVendors);
     }
-
-    [HttpGet("/vendors/{id}")]
-    public ActionResult Show(int id)
+    public ActionResult Create()
     {
-        Dictionary<string, object> model = new Dictionary<string, object>();
-        Vendor selectedVendor = Vendor.Find(id);
-        List<Order> vendorOrders = selectedVendor.VendorOrderList;
-        model.Add("vendor", selectedVendor);
-        model.Add("orders", vendorOrders);
-        return View("Orders", model);
+        return View();
     }
 
+    [HttpPost]
+    public ActionResult Create(Vendor vendor)
+    {
+        if (ModelState.IsValid)
+        {
+            new Vendor(vendor.Name);
+            return RedirectToAction("Index");
+        }
+        return View(vendor);
+    }
+
+    public ActionResult Details(int id)
+    {
+        Vendor vendor = Vendor.Find(id);
+        return View(vendor);
+    }
 }
